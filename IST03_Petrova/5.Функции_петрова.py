e3005bd1b164991469d07b00c6fe4dfa4c4774a1
MPetrova39 <mp4603534@gmail.com>
2026-03-02 00:24:06 +0300

    Add files via upload

---
+++ IST03_Petrova/5.Функции_петрова.py
@@ -0,0 +1,4 @@
+def create_spreadsheet(title, row_count=1000):
+    return 'Создание электронной таблицы с именем ' + title + ' with ' + str(row_count) + ' lines'
+
+print(create_spreadsheet('Приложения',10))
